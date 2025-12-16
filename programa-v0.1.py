4a2335986ac5265018b2cc20864897b007c3a737
EduardoGuerraL <eaguerra0@gmail.com>
2025-12-16 00:03:44 -0300

    Solo aviso de cambio de repositorio a donde Nuestro companero de grupo.

--- programa-v0.1.py
+++ programa-v0.1.py
@@ -1,6 +1,43 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from typing import Dict, List
+
+class Articulo:
+    def __init__(self, codigo, nombre, stock, precio):
+        self.codigo = codigo
+        self.nombre = nombre
+        self.stock = stock
+        self.precio = precio
+    
+    def agregar_stock(self, cantidad):
+        self.stock += cantidad
+    
+    def retirar_stock(self, cantidad):
+        self.stock -= cantidad
+
+    def __repr__(self):
+        return f"{self.codigo} - {self.nombre} | Stock: {self.stock} | Precio: {self.precio}"
+
+#Creo que esto podría ser mas facil si solo lo habrimos desde excel con pandas. Pero probemos:
+class Inventario:
+    def __init__(self):
+        self.articulos: Dict[str, Articulo] = {}
+    
+    def agregar_articulo(self, articulo:Articulo):
+        if articulo.codigo in self.articulos:
+            raise ValueError('El articulo ya existe')
+        self.articulos[articulo.codigo] == articulo
+    
+    def listar(self):
+        return [str(a) for a in self.articulos.values()]
+
+class SistenaInventario:
+    def __init__(self):
+        self.inventario = Inventario
+
+    def mostrarMenu(self):
+        pass
 
 def menu_principal():
 
@@ -66,3 +103,5 @@ def limpiar():
     os.system("cls" if os.name == "nt" else "clear")
 
 menu_principal()
+
+#Cambiandonos al repositorio del compańero
